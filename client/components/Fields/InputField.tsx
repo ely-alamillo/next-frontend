32630e76ada05e10365c444739743cf364c16015
Ely Alamillo <ely0013@gmail.com>
2019-01-30 21:34:12 -0600

    add register using formik

---
+++ client/components/Fields/InputField.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FieldProps } from "formik";
+
+type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
+
+export const InputField = ({
+  field,
+  form: _,
+  ...props
+}: FieldProps & InputProps) => {
+  return (
+    <div>
+      <input {...field} {...props} />
+    </div>
+  );
+};
