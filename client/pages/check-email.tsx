3eb525fa6549a36c03095a11bf7d76cdca5cbe53
Ely Alamillo <eleazar.alamillo@valic.com>
2019-02-05 14:33:09 -0600

    add check-email page

---
+++ client/pages/check-email.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+
+const checkEmail: React.FunctionComponent = () => (
+    <Layout title="Check Email">
+        <p>Check your email to confirm account.</p>
+    </Layout>
+)
+
+export default checkEmail;
