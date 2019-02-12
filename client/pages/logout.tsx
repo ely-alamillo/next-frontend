f549ef07eb14092b46fa3da0e3df9c8b8ae5ebd9
Ely Alamillo <eleazar.alamillo@valic.com>
2019-02-12 11:03:00 -0600

    add logout page

---
+++ client/pages/logout.tsx
@@ -0,0 +1,17 @@
+import { MyContext } from '../interfaces/MyContext';
+import { logoutMutation } from '../graphql/User/mutations/logout';
+import redirect from '../lib/redirect';
+
+const Logout = () => {
+  return null;
+};
+
+Logout.getInitialProps = async ({ apolloClient, ...ctx }: MyContext) => {
+  await apolloClient.mutate({ mutation: logoutMutation });
+  await apolloClient.resetStore();
+
+  redirect(ctx, '/login');
+  return {};
+};
+
+export default Logout;
